e2dd47d038c10b93b945ea593e5b35c7656a359a
Bonnie1005 <12750804@me.mcu.edu.tw>
2024-03-13 11:08:21 +0800

    week04

--- week01-1.cpp
+++
@@ -1,6 +0,0 @@
-// 這是 C/C++ 的程式碼，有點多
-# include <stdio.h>
-int main()
-{
-  print("Hello World\n");
-}
---
+++ week01/week01-1.cpp
@@ -0,0 +1,6 @@
+// 這是 C/C++ 的程式碼，有點多
+# include <stdio.h>
+int main()
+{
+  print("Hello World\n");
+}
